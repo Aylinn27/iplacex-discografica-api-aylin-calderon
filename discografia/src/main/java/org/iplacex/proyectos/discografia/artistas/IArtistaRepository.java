68ea5445d429a70a44b8debecebcac79ff7023ff
Aylinn27 <aylin.calderonn27@gmail.com>
2025-09-24 14:02:35 -0300

    Delete discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java

--- discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/IArtistaRepository.java
+++
@@ -1,6 +0,0 @@
-package org.iplacex.proyectos.discografia.artistas;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IArtistaRepository extends MongoRepository<Artista, String> {
-}
